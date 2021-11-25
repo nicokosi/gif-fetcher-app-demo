626f446630680bcb231ac6b08d4601d88502bca5
Nicolas Kosinski <nicokosi@users.noreply.github.com>
2021-11-25 15:43:43 +0100

    Initialize project from code sandbox ©
    Code comes from this [code sandbox](https://codesandbox.io/s/ts-pattern-gif-search-demo-n8h4k?file=/src/App.tsx).
    
    This a demo for
    [gvergnaud/ts-pattern](https://github.com/gvergnaud/ts-pattern).

---
+++ README.md
@@ -0,0 +1,14 @@
+# Gif fetcher app Demo
+
+[gvergnaud/ts-pattern](https://github.com/gvergnaud/ts-pattern)'s demo.
+
+Code comes from this [code sandbox](https://codesandbox.io/s/ts-pattern-gif-search-demo-n8h4k?file=/src/App.tsx).
+
+## Start locally
+
+`npm start` runs the app in the development mode.
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.
+You will also see any lint errors in the console.
---
+++ src/searchGif.tsx
@@ -0,0 +1,38 @@
+import { isMatching, __ } from "ts-pattern";
+import { API_KEY } from "./constants";
+
+const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+export function searchGif(query: string) {
+  return fetch(
+    `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`
+  )
+    .then((res) => res.json())
+    .then(async (res) => {
+      await delay(500);
+      return res;
+    })
+    .then((res: unknown) => {
+      if (
+        isMatching(
+          {
+            data: [
+              {
+                images: {
+                  fixed_height: {
+                    url: __.string
+                  }
+                }
+              }
+            ]
+          },
+          res
+        )
+      ) {
+        // Note how `isMatching` narrows the type of `res`:
+        return res.data.map(({ images }) => images.fixed_height.url);
+      } else {
+        throw new Error("Oh, no! The HTTP request failed.");
+      }
+    });
+}
